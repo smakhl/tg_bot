663d47645a0639e8d694c9fa5c15b1643eba9fe1
smakhl <38110833+smakhl@users.noreply.github.com>
2024-02-16 07:12:59 +0400

    allow explicit ai content generation

--- src/services/generateAiContent.ts
+++ src/services/generateAiContent.ts
@@ -25,19 +25,19 @@ export async function generateAiContent(prompt: string) {
     const safetySettings = [
         {
             category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
         },
         {
             category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
         },
         {
             category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
         },
         {
             category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
         },
     ]
 
