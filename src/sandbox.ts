79511ee6d6e21ce60d810d74191285df5408d5cc
smakhl <38110833+smakhl@users.noreply.github.com>
2024-02-16 07:27:52 +0400

    update sandbox

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,18 @@
-import { getScores } from './features/leaderboard/getScores.js'
-import { StatsSnapshot, getChallenge } from './services/db.js'
-import { SquadType } from './services/fortniteApi.js'
+import { getLeaderboard } from './features/leaderboard/getLeaderboard.js'
+import { getLeaderboardNarrative } from './features/leaderboard/getLeaderboardNarrative.js'
+import { getLeaderboardPrompt } from './features/leaderboard/getLeaderboardPrompt.js'
 
-const challenge = await getChallenge(Number(process.env.TEST_CHAT_ID))
-if (!challenge) process.exit()
+const leaderboard = await getLeaderboard(Number(process.env.TEST_CHAT_ID))
+if (!leaderboard) process.exit()
 
-const prevPrevStats: StatsSnapshot = challenge.history.at(-3)!
-const prevStats: StatsSnapshot = challenge.history.at(-2)!
-const freshStats: StatsSnapshot = challenge.history.at(-1)!
+let leaderboardPrompt = getLeaderboardPrompt(
+    leaderboard.scores,
+    leaderboard.prevScores
+)
 
-const scores = getScores({
-    prevStats,
-    freshStats,
-    squadType: SquadType.squad,
-    players: challenge.players,
-}).scores
-console.log('🚀 ~ run ~ scores:', scores)
+const narrative = await getLeaderboardNarrative(
+    leaderboardPrompt,
+    leaderboard.updatedChallenge.instruction_for_narrative
+)
 
-let prevScores = getScores({
-    prevStats: prevPrevStats,
-    freshStats: prevStats,
-    squadType: SquadType.squad,
-    players: challenge.players,
-}).scores
-console.log('🚀 ~ run ~ prevScores:', prevScores)
+console.log(narrative)
